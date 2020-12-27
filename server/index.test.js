1ccf38fe2dfb5af1c490fa89d3d0c209840b17a2
Ricky Rhodes <ricky@rickyrhodes.com>
2020-12-27 12:09:45 -0500

    passed

--- server/index.test.js
+++ server/index.test.js
@@ -12,7 +12,7 @@ describe('GET /users', () => {
 
   it('should return list of users', async () => {
     const res = await request(app).get('/api/users');
-    expect(res.status).to.equal(404);
+    expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(seedUsers.length);
   });
 });
